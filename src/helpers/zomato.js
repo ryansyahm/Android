5d6e846249bfd8d57084a2d4737f67d218d1a00d
ryansyahm <fikriansyah51@gmail.com>
2021-02-19 01:03:30 +0700

    day-7

--- src/helpers/zomato.js
+++ src/helpers/zomato.js
@@ -1,2 +1,2 @@
-export const ZOMATO_KEY = 'd3b71f27721be564f7a98cc5d01bd9d4';
+export const ZOMATO_KEY = 'e77d4120c37040f81a2039d8022a1e06';
 export const ZOMATO_URL = 'https://developers.zomato.com/api/v2.1';
